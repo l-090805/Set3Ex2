4137222103d3765fd706ccf431f208ce42b5c02f
falau <falau@LENOVO>
2024-12-16 19:15:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Set3Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("vector de numere: ");
+            string input = Console.ReadLine();
+            int[] v = Array.ConvertAll(input.Split(' '), int.Parse);
+            Console.Write("Numar k: ");
+            int k = int.Parse(Console.ReadLine());
+
+            int poz = 0;
+
+            if (v.Contains(k)) 
+            {
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] != k)
+                {
+                    poz++;
+                }
+            }
+                Console.WriteLine(poz);
+
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
